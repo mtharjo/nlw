55e3b75563653f347a0fcac9ca1dcdcaf552b40c
mtharjoo <valdir_matheus@hotmail.com>
2020-09-17 22:11:53 -0300

    projeto NLW da Rocketseat

---
+++ src/database/fake_data.js
@@ -0,0 +1,24 @@
+const proffys = [
+    { 
+        name:"Diego Fernandes", 
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "11993288260", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0] , 
+        time_from: [720] , 
+        time_to: [1220] 
+    },
+    { 
+        name:"Matheus Araujo",
+        avatar:"https://scontent.fcgh24-1.fna.fbcdn.net/v/t1.0-9/116288348_1638208786326187_7962361293499488704_o.jpg?_nc_cat=106&_nc_sid=8bfeb9&_nc_eui2=AeG9aGU0HHOil0gLUKVJfIbdITGYqXpZjbMhMZipelmNs9jetzFba7j8wpvyCn8ufBinT1znWyY8KlvuKE18yYsC&_nc_ohc=kFWLctuOxsAAX822ORB&_nc_ht=scontent.fcgh24-1.fna&oh=b91523861c214afa77a05c46ee23a3ba&oe=5F5302ED",
+        whatsapp: "11993288260", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1] , 
+        time_from: [720] , 
+        time_to: [1220] 
+    }
+]
